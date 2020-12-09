fa272bee646a71836eb06ed9c25175957e8c843f
Edison Ucsiña <edisonucsina@gmail.com>
2020-12-09 17:47:19 -0500

    Cambios en la Navegacion y en las cards

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,29 +1,32 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import logo from '../img/logo.png';
+import '../css/Navigation.css';
 
 export default function Navigation() {
     return (
         
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark ">
               <div className="container">
-    <Link className="navbar-brand" to="/smooking-nature/">Inicio</Link>
-    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNavDropdown">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <Link className="nav-link" to="/smooking-nature/about">¿QUIÉNES SOMOS?</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/smooking-nature/contact">CONTACTOS</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/smooking-nature/product">PRODUCTOS</Link>
-        </li>
-      </ul>
-    </div>
+                <Link className="navbar-brand" to="/smooking-nature/"> <img src={logo} className="logotipo" alt="logo"></img></Link>
+                  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+
+                <div className="collapse navbar-collapse" id="navbarNavDropdown">
+                  <ul className="navbar-nav navbar-right">
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/smooking-nature/about">¿QUIÉNES SOMOS?</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/smooking-nature/contact">CONTACTOS</Link>
+                    </li>
+                    <li className="nav-item">
+                      <Link className="nav-link" to="/smooking-nature/product">PRODUCTOS</Link>
+                    </li>
+                  </ul>
+                </div>
     </div>
   </nav>
 

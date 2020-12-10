1c1d9babb737679d5fde07a77808124986f1876d
Edison Ucsiña <edisonucsina@gmail.com>
2020-12-10 16:44:12 -0500

    Mapa en la el Componente Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -42,23 +42,27 @@ export default function Contact() {
 
                     <div className="col-sm-4">
                         <h4>Información de Contacto</h4>
-                    <p><i class="fas fa-phone"></i><b> Teléfono:</b> (02) 3411730</p>
+                    <p><i className="fas fa-phone"></i><b> Teléfono:</b> (02) 3411730</p>
 
-                    <p><i class="fab fa-whatsapp"></i><b> Whatsapp:</b> 0995154854</p>
+                    <p><i className="fab fa-whatsapp"></i><b> Whatsapp:</b> 0995154854</p>
 
                     <hr></hr>
                     <h4>Horario de Atención</h4>
                     
-                    <p><i class="fas fa-clock"></i> Lunes a Viernes de 8H00 a 16H30, Sabados de 9H00 a 14H00.</p>
+                    <p><i className="fas fa-clock"></i> Lunes a Viernes de 8H00 a 16H30, Sabados de 9H00 a 14H00.</p>
 
                     <hr></hr>
                     <h4>Donde nos ubicamos</h4>
-                    <p><i class="fas fa-map-marker-alt"></i><b> Dirección:</b> De las Madreselvas N48-50 y De las Hortencias, Sector El Inca.</p>
-                                 
-                    </div>
-                    
-                </div>
+                    <p><i className="fas fa-map-marker-alt"></i><b> Dirección:</b> De las Madreselvas N48-50 y De las Hortencias, Sector El Inca.</p>          
+                        
+                      
+                        <iframe title="Contact Map" className="contact-map center-block" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.8024460604784!2d-78.49910578601666!3d-0.1616248998820705!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d59ab8b2baefef%3A0xc3968ebe435c5e8c!2sCentro%20Comercial%20El%20Bosque!5e0!3m2!1ses!2sus!4v1607635263767!5m2!1ses!2sus"></iframe>
+                        
+
+                </div>     
             </div>
+            <br/>
+        </div>
             
             <Footer></Footer>
         </div>
